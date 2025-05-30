62353bf9a58cac783a4e4780d0375a4cf3e79681
Falgor <tranhoa.7604@gmail.com>
2025-05-30 14:13:26 +0700

    add: route

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -4,4 +4,11 @@ describe('app', () => {
     cy.get('h1').contains('Layout');
     cy.get('h2').contains('Home');
   });
+
+  it('route', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('h2').contains('Home');
+    cy.contains('About').click();
+    cy.get('h2').contains('About');
+  });
 });
---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+ 
+export default function Page() {
+  return (
+    <div>
+      <h2>About</h2>
+      <Link href="/">Home</Link>
+    </div>
+  )
+}
