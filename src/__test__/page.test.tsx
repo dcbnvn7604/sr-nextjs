8b3ecf5b75785b63a604e83686ffa1f61a2192fa
Falgor <tranhoa.7604@gmail.com>
2025-06-04 13:45:26 +0700

    add: validation

--- src/__test__/page.test.tsx
+++ src/__test__/page.test.tsx
@@ -1,13 +1,28 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, prettyDOM } from '@testing-library/react'
 import Page from '@/app/page'
+import { act } from 'react'
  
 describe('Page', () => {
-  it('renders a heading', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn()
+  })
+
+  it('renders a heading',async () => {
+    (fetch as jest.Mock).mockResolvedValueOnce({
+      status: 401,
+      json: async () => ({detail: [{field: "username", message: "User existed"}]})
+    })
     render(<Page />)
- 
-    const heading = screen.getByRole('heading', { level: 1 })
- 
-    expect(heading).toBeInTheDocument()
+    const button = screen.getByTestId('submit')
+    fireEvent.click(button)
+    await screen.findByText("Username is required")
+    const username = screen.getByLabelText('username')
+    fireEvent.change(username, {target: {value: "user1"}})
+    fireEvent.click(button)
+    await screen.findByText("Invalid email address")
+    fireEvent.change(username, {target: {value: "user1@com.com"}})
+    fireEvent.click(button)
+    await screen.findByText("User existed")
   })
 })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,47 @@
-import Link from 'next/link'
+'use client'
+import { Box, Button, TextField } from '@mui/material';
+import { useForm } from 'react-hook-form'
  
 export default function Page() {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data: object) => {
+    const response = await fetch('http://localhost:3000/api/users', {
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+    if (response.status == 401) {
+      const result = await response.json()
+      result["detail"].forEach((error:{field:string, message: string}) => {
+        setError(error["field"], {
+          type: "manual",
+          message: error["message"]
+        })
+      })
+    }
+  }
+
   return (
-    <div>
-      <h2>Home</h2>
-      <Link href="/about">About</Link>
-    </div>
+    <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        data-testid="username"
+        label="username"
+        {...register('username', {
+          required: 'Username is required',
+          pattern: {
+            value: /^\S+@\S+$/i,
+            message: 'Invalid email address'
+          }
+        })}
+        error={!!errors.username}
+        helperText={errors.username?.message as string}
+      />
+      <Button data-testid="submit" type="submit">Register</Button>
+    </Box>
   )
 }
