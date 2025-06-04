b076e94d038c61e5b9ce5272270293d11ce55467
Falgor <tranhoa.7604@gmail.com>
2025-06-04 14:26:23 +0700

    add: list_item

--- src/__test__/page.test.tsx
+++ src/__test__/page.test.tsx
@@ -1,13 +1,20 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import Page from '@/app/page'
  
 describe('Page', () => {
-  it('renders a heading', () => {
-    render(<Page />)
+  beforeEach(() => {
+    global.fetch = jest.fn()
+  })
+
+  it('renders a heading', async () => {
+    (fetch as jest.Mock).mockResolvedValueOnce({
+      json: async () => ([{username: 'username1'}, {username: 'username2'}])
+    })
+    const { container } = render(<Page />)
  
-    const heading = screen.getByRole('heading', { level: 1 })
- 
-    expect(heading).toBeInTheDocument()
+    await waitFor(() => {
+      expect(container.querySelectorAll('li').length).toEqual(2)
+    })
   })
 })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
  
 export default function Page() {
+  const [users, setUsers] = useState<{username: string}[]>([])
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('http://localhost:3000/api/users')
+      const result = await response.json()
+      setUsers(result)
+    })()
+  },[])
   return (
     <div>
-      <h2>Home</h2>
-      <Link href="/about">About</Link>
+      <h2>User list</h2>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>{user.username}</li>
+        ))}
+      </ul>
     </div>
   )
 }
